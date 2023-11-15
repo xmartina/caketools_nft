c6c90b6b909dddcd2d59ffadbe955f7de174254c
xmartina <martinsprince126@gmail.com>
2023-11-15 09:02:19 +0100

    init

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php require_once('header.php'); ?>
 
 <?php
-echo $_SERVER['REQUEST_URI'];
-    if($_SERVER['REQUEST_URI'] == ''){
+    if($_SERVER['REQUEST_URI'] == '/'){
         echo require_once ('main.php');
     }
 ?>
