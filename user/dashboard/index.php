1703152e76c17315167d0b7b3989b175b7af8a01
xmartina <martinsprince126@gmail.com>
2023-11-16 10:27:51 +0100

    init

--- user/dashboard/index.php
+++ user/dashboard/index.php
@@ -3,13 +3,13 @@ const basePath = '/home/multistream6/domains/caketoolsnftmarketplace.com/user';
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 // Start the session
-//session_start();
+session_start();
 
 // Check if the user is logged in, otherwise redirect to the login page
-//if (!isset($_SESSION['user_id'])) {
-//    header("Location: /user/sign-in");
-//    exit();
-//}
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /user/sign-in");
+    exit();
+}
 
 // Include any necessary header or layout files
 
