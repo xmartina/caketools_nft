27739e613d34e0384da9dc33745acedd317bc6fd
xmartina <martinsprince126@gmail.com>
2023-11-16 07:24:45 +0100

    init

--- user/dashboard/index.php
+++ user/dashboard/index.php
@@ -3,7 +3,7 @@ const basePath = '/home/multistream6/domains/caketoolsnftmarketplace.com/user';
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 // Start the session
-session_start();
+//session_start();
 
 // Check if the user is logged in, otherwise redirect to the login page
 //if (!isset($_SESSION['user_id'])) {
