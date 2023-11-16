147a815cd14d6f9fc62d854d90f333639a5be338
xmartina <martinsprince126@gmail.com>
2023-11-16 07:48:20 +0100

    init

---
+++ user/dashboard/config.php
@@ -0,0 +1,5 @@
+<?php
+const siteName = 'Caketools NFT Marketplace';
+const siteLogo = '/asset/img/logo.png';
+const siteIcon = '/asset/img/icon.png';
+const siteUrl = 'https://caketoolsnftmarketplace.com';
