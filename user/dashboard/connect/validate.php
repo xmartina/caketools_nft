55647338bdd20e72c611b8fe83021cd0fedeb5c6
xmartina <martinsprince126@gmail.com>
2023-11-17 13:05:24 +0100

    init

--- user/dashboard/connect/validate.php
+++ user/dashboard/connect/validate.php
@@ -617,7 +617,7 @@
     z-index:999;
     top:10%;
     left:9px;
-    background-color:#0ba360;
+    background-color: #02042c;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
     color:#fff;
     padding:12px 15px;
