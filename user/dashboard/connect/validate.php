19d49094db8dd252bc15e31be95664955f93896b
xmartina <martinsprince126@gmail.com>
2023-11-17 08:18:20 +0100

    init

--- user/dashboard/connect/validate.php
+++ user/dashboard/connect/validate.php
@@ -1071,7 +1071,7 @@ okay this a notification
 
     }
     html .img_icons{
-      padding: 4px;
+      padding: 14px;
       background-color: #d4dae5;
       border-radius:7px;
       box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
