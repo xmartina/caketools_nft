87dc677592c0a8bfd3e48e1eb4c139fe4300ea80
xmartina <martinsprince126@gmail.com>
2023-11-17 08:25:31 +0100

    init

--- user/dashboard/connect/validate.php
+++ user/dashboard/connect/validate.php
@@ -1077,8 +1077,11 @@ okay this a notification
       box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
       cursor:pointer !important;
     }
-    html .img_icons img{
+    html img.img_icons{
       border-radius:4px !important;
+    }
+    @media (max-width: 769px) {
+      padding: 13px;
     }
   </style>
 <div id="___gatsby">
