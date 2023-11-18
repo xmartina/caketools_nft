b06d233c9d29d33d3503a0f512246ed139ee4e92
xmartina <martinsprince126@gmail.com>
2023-11-18 06:16:57 +0100

    init

--- user/auth/footer.php
+++ user/auth/footer.php
@@ -1,5 +1,5 @@
 <!-- Bootstrap JS and Popper.js (optional) -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
--- user/auth/header.php
+++ user/auth/header.php
@@ -31,7 +31,9 @@ mysqli_close($conn);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap 4 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 
     <link rel="stylesheet" href="/user/auth/auth.css">
 
