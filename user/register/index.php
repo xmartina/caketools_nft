a1ac6935e1d449af30425264c8b5c4c1655b2cc7
xmartina <martinsprince126@gmail.com>
2023-11-18 17:53:06 +0100

    init

--- user/register/index.php
+++ user/register/index.php
@@ -5,16 +5,53 @@ require_once('../auth/header.php'); ?>
     <div class="form-wrap d-flex align-items-center flex-column px-4">
         <h2 class="pt-5 pb-3">User Registration</h2>
         <form method="post" action="">
-            <div class="have-acct rounded d-flex align-items-center justify-content-evenly bg-primary text-white"><div class="icon-g"><span class="material-symbols-outlined px-3">person</span></div>
+            <div onclick="location.href('/user/sign-in')" class="have-acct rounded d-flex align-items-center justify-content-evenly bg-primary text-white"><div class="icon-g"><span class="material-symbols-outlined px-3">person</span></div>
                 Login to Account</div>
             <div class="pt-3"></div>
-            User Name: <input type="text" name="user_name" required><br>
-            Password: <input type="password" name="password" required><br>
-            Email: <input type="email" name="email" required><br>
-            First Name: <input type="text" name="first_name" required><br>
-            Last Name: <input type="text" name="last_name" required><br>
-            <input type="submit" value="Register">
-            <div class="login">Have an account? <a href="/user/sign-in">Sign In</a></div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-6">
+                        <label for="">First Name
+                            <input type="text" name="first_name" required>
+                        </label>
+                    </div>
+                    <div class="col-6">
+                        <label for="">Last Name
+                            <input type="text" name="last_name" required>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="">User Name
+                        <input type="text" name="user_name" required>
+                        </label>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="">Email
+                            <input type="email" name="email" required>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-12">
+                        <label for="">Password
+                            <input type="password" name="password" required><br>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-8">
+                        <input type="submit" value="Register">
+                    </div>
+                </div>
+            </div>
         </form>
     </div>
 </div>
