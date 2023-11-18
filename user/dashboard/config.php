c997adfafc7293867bc16e937dd4448d4d2ada15
xmartina <martinsprince126@gmail.com>
2023-11-19 00:51:11 +0100

    init

--- user/dashboard/config.php
+++ user/dashboard/config.php
@@ -3,3 +3,21 @@ const siteName = 'Caketools NFT Marketplace';
 const siteLogo = '/asset/img/logo.png';
 const siteIcon = '/asset/img/icon.png';
 const siteUrl = 'https://caketoolsnftmarketplace.com';
+
+
+if (isset($_POST['logout'])){
+
+    session_start();
+
+// Unset all session variables
+    $_SESSION = array();
+
+// Destroy the session
+    session_destroy();
+
+// Redirect to the login page or any other desired page after logout
+    header("Location: /user/sign-in");
+    exit();
+
+
+}
