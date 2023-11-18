60e33707bc9c4a3fdabce44585a8f8e887f49432
xmartina <martinsprince126@gmail.com>
2023-11-18 17:36:28 +0100

    init

--- user/register/index.php
+++ user/register/index.php
@@ -2,10 +2,10 @@
 $pageTitle = 'User Registration';
 require_once('../auth/header.php'); ?>
 <div class="main-form d-flex align-items-center justify-content-center">
-    <div class="form-wrap d-flex align-items-center flex-column px-4 pt-5">
+    <div class="form-wrap d-flex align-items-center flex-column px-4">
         <h2 class="py-5">User Registration</h2>
         <form method="post" action="">
-            <div class="have-acct rounded d-flex align-items-center justify-content-evenly bg-primary text-white"><span class="material-symbols-outlined">person</span>
+            <div class="have-acct rounded d-flex align-items-center justify-content-evenly bg-primary text-white"><div class="icon-g"><span class="material-symbols-outlined px-3">person</span></div>
                 Login to Account</div>
             <div class="pt-3"></div>
             User Name: <input type="text" name="user_name" required><br>
