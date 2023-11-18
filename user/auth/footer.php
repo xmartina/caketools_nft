d24fccdb84ed9a4e105c9723cdb0915a1854c64b
xmartina <martinsprince126@gmail.com>
2023-11-18 06:10:54 +0100

    init

--- user/auth/footer.php
+++ user/auth/footer.php
@@ -1,2 +1,5 @@
+<!-- Bootstrap JS and Popper.js (optional) -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
